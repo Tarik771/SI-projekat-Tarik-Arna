1ecc6d6c0427bccdd5e99c316dac989af6454bd2
Tarik771 <159940942+Tarik771@users.noreply.github.com>
2024-03-03 18:22:54 +0100

    Prva funkcionalnost uradjena

---
+++ SI_E_ucenje/js/home.js
@@ -0,0 +1,11 @@
+var loginBtn = document.getElementById("login-btn");
+var registerBtn = document.getElementById("register-btn");
+
+loginBtn.addEventListener("click", function () {
+    window.location.href = 'login.html';
+});
+
+registerBtn.addEventListener("click", function () {
+    window.location.href = 'register-start.html';
+});
+
